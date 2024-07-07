1e9e7833773c26fe3e031a4ced32c78705022b77
AntonioPivaldi <9212565@gmail.com>
2024-07-08 00:53:15 +0300

    feat: add styles to error boundary

--- src/ErrorBoundary.tsx
+++ src/ErrorBoundary.tsx
@@ -17,7 +17,21 @@ class ErrorBoundary extends React.Component<PropsWithChildren, BoundaryState> {
 
   render() {
     if (this.state.errorMessage) {
-      return <p className="p-6">{this.state.errorMessage}</p>
+      return (
+        <div className="flex flex-col pl-40 gap-6 h-dvh bg-[#0074D0] text-white">
+          <div className="mt-28">
+            <p className="text-[10rem]">{':('}</p>
+            <div className="text-[2rem]">
+              <p>Error message: "{this.state.errorMessage}"</p>
+              <p>
+                {
+                  'Lorem ipsum, yada yada. Please, be more careful next time! :)'
+                }
+              </p>
+            </div>
+          </div>
+        </div>
+      )
     }
 
     return this.props.children
